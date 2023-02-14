e199952e97ae7068c0a36894fec804a8a433d49a
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-02-14 03:08:54 -0800

    added function to remove rental from cart

--- ecommerce-ui/src/ShoppingCartCard.jsx
+++ ecommerce-ui/src/ShoppingCartCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-function ShoppingCartCard({ rental }) {
-  const { title, houseType, image, payment } = rental;
-  console.log(rental);
+function ShoppingCartCard({ rental, removeFromCart }) {
+  const { title, houseType, image, payment } = rental.cartRental;
+  // console.log(`shopping card no rental: ${JSON.stringify(rental)}`);
+  // console.log(`shopping card cartRental: ${JSON.stringify(rental.cartRental)}`);
   return (
     <div className='shopping-cart-card'>
     <div className="shopping-cart-image-container">
@@ -12,6 +13,11 @@ function ShoppingCartCard({ rental }) {
       <p className="shopping-cart-housetype">{houseType}</p>
       <p className="shopping-cart-title">{title}</p>
       <p className="shopping-cart-price">${payment.cost}</p>
+      <div className='vacation-button-container'>
+        <button className="remove-button"
+          onClick={() => removeFromCart(rental)}
+        >REMOVE</button>
+      </div>
     </div>
   </div>
   )
